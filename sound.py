46d02d238aa18ae5046bfc1001c8a5a7ddf1b76c
CynthiaVN2016 <mscynthiavu@gmail.com>
2019-12-12 03:53:02 -0500

    Add more songs and change amplitude

--- sound.py
+++ sound.py
@@ -5,14 +5,30 @@ import serial
 ser = serial.Serial('/dev/tty.SLAB_USBtoUART', 9600)
 s = [0]
 print("Starting while loop\n")
-marioPattern = [6, 6, 6, 4, 6, 8, 1]
+marioPattern = [7, 7, 7, 4, 7, 9, 1]
+fairyPattern = [8, 6, 8, 7, 5, 5]
+christmasPattern = [4, 8, 8, 9, 8, 7, 6, 6]
 pattern = []
 
 def PlayMario():
 	notes = [C5,  G4,   E4,   A4,   B4,  Bb4,   A4,  G4,  E5,   G5,  A5,    F5,   G5,   E5,  C5, D5, B4]
 	time =  [0.5, 0.5, 0.5, 0.35, 0.35, 0.15, 0.25, .25, .25, 0.25, 0.35, 0.15, 0.35, 0.35, 0.15, 0.15, 0.15]
 	for i in range(0, len(notes)):
-		play(notes[i], amp=0.25)
+		play(notes[i], amp=0.3)
+		sleep(time[i])
+
+def PlaySugarPlum():
+	notes = [ C5,   C5,  C5,   B4,   B4,  B4,  Bb4,  Bb4,  Bb4,   G4,   D5,  Bb4,   D5, A4]
+	time = [0.35, 0.35, 0.5, 0.35, 0.35, 0.5, 0.35, 0.35,  0.5, 0.35, 0.35, 0.35, 0.35, 1]
+	for i in range(0, len(notes)):
+		play(notes[i], amp=0.3)
+		sleep(time[i])
+
+def PlayChristmas():
+	notes = [D5,  G5,   G5,   A5,   G5,   F5,   E5,  C5,   C5,  A5,   A5,  Bb5,   A5,   G5,   F5,  D5,   C5,   C5,  D5, G5,   E5,  F5]
+	time = [0.5, 0.5, 0.25, 0.25, 0.25, 0.25,  0.5, 0.5,  0.5, 0.5, 0.25, 0.25, 0.25, 0.25,  0.5, 0.5, 0.25, 0.25, 0.5, 0.5, 0.5, 0.5]
+	for i in range(0, len(notes)):
+		play(notes[i], amp=0.3)
 		sleep(time[i])
 
 while True:
@@ -20,33 +36,44 @@ while True:
 	print(read_serial)
 	# s[0] = str(int (ser.readline(), 16))
 	# print s[0]
+
 	if (read_serial == 1):
-		play(G4)
+		play(G4, amp=0.3)
 	elif (read_serial == 2):
-		play(A4)
+		play(A4, amp=0.3)
 	elif (read_serial == 3):
-		play(B4)
+		play(B4, amp=0.3)
 	elif (read_serial == 4):
-		play(C5)
+		play(C5, amp=0.3)
 	elif (read_serial == 5):
-		play(D5)
+		play(Cs5, amp=0.3)
 	elif (read_serial == 6):
-		play(E5)
+		play(D5, amp=0.3)
 	elif (read_serial == 7):
-		play(F5)
+		play(E5, amp=0.3)
 	elif (read_serial == 8):
-		play(G5)
+		play(F5, amp=0.3)
 	elif (read_serial == 9):
-		play(A5)
+		play(G5, amp=0.3)
 
 	print(len(pattern))
-	if (len(pattern) > 6):
+	if (len(pattern) > 7):
 		pattern.pop(0)
 	pattern.append(read_serial)
 	print(pattern)
 
-	if (pattern == marioPattern):
+	if (pattern == christmasPattern):
+		sleep(0.5)
+		PlayChristmas()
+		pattern = []
+	elif (pattern[1:] == marioPattern):
 		sleep(0.5)
 		PlayMario()
+		pattern = []
+	elif (pattern[2:] == fairyPattern):
+		sleep(0.5)
+		PlaySugarPlum()
+		pattern = []
+
 
 
